2f2144eb29236018a6d68290684dca7e90cbe79c
bala-amypo <bala@amypo.in>
2025-12-19 16:06:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,33 +3,27 @@ package com.example.demo.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.LoginRequest;
-import com.example.demo.dto.RegisterRequest;
-import com.example.demo.exception.DuplicateResourceException;
+import com.example.demo.entity.User;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Override
-    public String registerUser(RegisterRequest request) {
+    private final UserRepository repo;
 
-        // example duplicate check
-        if ("admin@gmail.com".equals(request.getEmail())) {
-            throw new DuplicateResourceException("Email already exists");
-        }
-
-        return "User registered successfully";
+    public UserServiceImpl(UserRepository repo) {
+        this.repo = repo;
     }
 
     @Override
     public String login(LoginRequest request) {
 
-        if (!"admin@gmail.com".equals(request.getEmail())) {
-            throw new ResourceNotFoundException("User not found");
-        }
+        User user = repo.findByEmail(request.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        if (!"admin".equals(request.getPassword())) {
+        if (!user.getPassword().equals(request.getPassword())) {
             throw new ResourceNotFoundException("Invalid password");
         }
 
