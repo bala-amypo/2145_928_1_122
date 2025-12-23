a976371da29cf627669a6ba6fd50cd1822f6db54
bala-amypo <bala@amypo.in>
2025-12-23 09:51:17 +0000

    User commit

--- src/main/java/com/example/demo/service/SaleTransactionService.java
+++ src/main/java/com/example/demo/service/SaleTransactionService.java
@@ -8,7 +8,8 @@
 //     SaleTransaction insertSale(SaleTransaction s);
 //     Optional<SaleTransaction> getSaleById(Long id);
 //     List<SaleTransaction> getSalesByCode(Long codeId);
-// }package com.example.demo.service;
+// }
+package com.example.demo.service;
 
 import java.util.List;
 import com.example.demo.model.SaleTransaction;
